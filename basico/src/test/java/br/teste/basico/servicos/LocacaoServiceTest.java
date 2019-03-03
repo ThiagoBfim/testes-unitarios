042de792e83f84bd315c65ef28937961c2dacc3f
Thiago Bomfim <thiagobomfim1995@gmail.com>
2019-03-03 11:42:51 -0300

    [UPDATE] - Incluindo a regra do Isolate, para isolar o comportamento de cada teste.

--- basico/src/test/java/br/teste/basico/servicos/LocacaoServiceTest.java
+++ basico/src/test/java/br/teste/basico/servicos/LocacaoServiceTest.java
@@ -16,14 +16,24 @@ public class LocacaoServiceTest {
 
     /**
      * O teste precisa ser FIRST
-     * Fast				(Menos de 1 segundo)
-     * Independent		(Não depender de outros testes)
-     * Repeatable		(Retornar o mesmo resultado indepdentemente de quantas vezes for executado)
-     * Self-verifying	(O teste precisa se auto avaliar, e informar se houver erro ou não)
-     * Timely			(Deve ser criada no momento certo.)
+     * Fast				        (Menos de 1 segundo)
+     * Independent/Isolate		(Não depender de outros testes) (Cada teste deve testar uma unica coisa de forma isolada.)
+     * Repeatable		        (Retornar o mesmo resultado indepdentemente de quantas vezes for executado)
+     * Self-verifying	        (O teste precisa se auto avaliar, e informar se houver erro ou não)
+     * Timely			        (Deve ser criada no momento certo.)
      */
     @Test
-    public void testarAlugarFilme() {
+    public void testarDataLocacao() {
+        LocacaoService locacaoService = new LocacaoService();
+        Filme filme = new Filme();
+        filme.setPrecoLocacao(10.50);
+        Locacao locacao = locacaoService.alugarFilme(null, filme);
+        assertTrue(isMesmaDataSimples(locacao.getDataRetorno(), adicionarDias(new Date(), 1)));
+        assertTrue(isMesmaDataSimples(locacao.getDataLocacao(), new Date()));
+    }
+
+    @Test
+    public void testarValorLocacao() {
         LocacaoService locacaoService = new LocacaoService();
         Filme filme = new Filme();
         filme.setPrecoLocacao(10.50);
@@ -31,7 +41,5 @@ public class LocacaoServiceTest {
         assertEquals(locacao.getValor(), filme.getPrecoLocacao());
         assertThat(locacao.getValor(), org.hamcrest.CoreMatchers.is(10.50));
         assertThat(locacao.getValor(), is(not(10.501)));
-        assertTrue(isMesmaDataSimples(locacao.getDataRetorno(), adicionarDias(new Date(), 1)));
-        assertTrue(isMesmaDataSimples(locacao.getDataLocacao(), new Date()));
     }
 }
