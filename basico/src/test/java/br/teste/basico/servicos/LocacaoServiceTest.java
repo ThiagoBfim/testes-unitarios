3a294b368c25c0a3e1445161d491fc8484efed27
Thiago Bomfim <thiagobomfim1995@gmail.com>
2019-03-03 11:30:48 -0300

    [UPDATE] - Melhorando o teste unitario.

--- basico/src/test/java/br/teste/basico/servicos/LocacaoServiceTest.java
+++ basico/src/test/java/br/teste/basico/servicos/LocacaoServiceTest.java
@@ -2,11 +2,15 @@ package br.teste.basico.servicos;
 
 import br.teste.basico.entidades.Filme;
 import br.teste.basico.entidades.Locacao;
-import br.teste.basico.utils.DataUtils;
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Date;
+
+import static br.teste.basico.utils.DataUtils.adicionarDias;
+import static br.teste.basico.utils.DataUtils.isMesmaDataSimples;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.*;
 
 public class LocacaoServiceTest {
 
@@ -24,8 +28,10 @@ public class LocacaoServiceTest {
         Filme filme = new Filme();
         filme.setPrecoLocacao(10.50);
         Locacao locacao = locacaoService.alugarFilme(null, filme);
-        Assert.assertEquals(locacao.getValor(), (filme.getPrecoLocacao()));
-        Assert.assertTrue(DataUtils.isMesmaDataSimples(locacao.getDataRetorno(),DataUtils.adicionarDias(new Date(), 1)));
-        Assert.assertTrue(DataUtils.isMesmaDataSimples(locacao.getDataLocacao(),new Date()));
+        assertEquals(locacao.getValor(), filme.getPrecoLocacao());
+        assertThat(locacao.getValor(), org.hamcrest.CoreMatchers.is(10.50));
+        assertThat(locacao.getValor(), is(not(10.501)));
+        assertTrue(isMesmaDataSimples(locacao.getDataRetorno(), adicionarDias(new Date(), 1)));
+        assertTrue(isMesmaDataSimples(locacao.getDataLocacao(), new Date()));
     }
 }
